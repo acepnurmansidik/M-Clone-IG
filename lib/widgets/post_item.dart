c1c05134ff666dcd688beae7cfd52b02a5565ae4
acep nurman sidik <acepnurmansidik@gmail.com>
2025-01-07 15:18:54 +0700

    update post item

--- lib/widgets/post_item.dart
+++ lib/widgets/post_item.dart
@@ -1,3 +1,6 @@
+// ignore_for_file: unused_local_variable, unused_element, avoid_print
+
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:clone_instagram/theme.dart';
 import 'package:clone_instagram/widgets/icon_wrap.dart';
 import 'package:clone_instagram/widgets/profile_item.dart';
@@ -12,27 +15,35 @@ class PostItem extends StatefulWidget {
 
 class _PostItemState extends State<PostItem> {
   ScrollController controller = ScrollController();
+
   @override
   Widget build(BuildContext context) {
+    bool isShowDesc = true;
+    int currentIndex = 0;
+
     Widget iconBtnWithDesc({
       required String iconName,
       required String counting,
+      required Function() onPressed,
     }) {
-      return Container(
-        margin: const EdgeInsets.only(right: 5),
-        child: Row(
-          children: [
-            IconWrap(
-              color: kBlackColor,
-              iconName: iconName,
-              size: 27,
-              margin: const EdgeInsets.only(right: 5),
-            ),
-            Text(
-              counting,
-              style: whiteTextStyle.copyWith(color: kBlackColor),
-            )
-          ],
+      return GestureDetector(
+        onTap: onPressed,
+        child: Container(
+          margin: const EdgeInsets.only(right: 5),
+          child: Row(
+            children: [
+              IconWrap(
+                color: kBlackColor,
+                iconName: iconName,
+                size: 27,
+                margin: const EdgeInsets.only(right: 5),
+              ),
+              Text(
+                counting,
+                style: whiteTextStyle.copyWith(color: kBlackColor),
+              )
+            ],
+          ),
         ),
       );
     }
@@ -45,6 +56,7 @@ class _PostItemState extends State<PostItem> {
         decoration: BoxDecoration(
           border: Border(
             bottom: BorderSide(width: 1, color: kGreyColor.withOpacity(.08)),
+            top: BorderSide(width: 1, color: kGreyColor.withOpacity(.08)),
           ),
         ),
         child: Row(
@@ -53,81 +65,111 @@ class _PostItemState extends State<PostItem> {
             Row(
               children: [
                 const ProfileItem(size: 40),
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Text(
-                      'Acepnurmansidik_',
-                      style: blackTextStyle.copyWith(
-                        fontSize: 12,
-                        fontWeight: semibold,
-                      ),
-                    ),
-                  ],
+                Text(
+                  'Acepnurmansidik_',
+                  style: blackTextStyle.copyWith(
+                    fontSize: 12,
+                    fontWeight: semibold,
+                  ),
                 ),
               ],
             ),
-            Container(
-              height: 60,
-              decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                image: DecorationImage(
-                  colorFilter: ColorFilter.mode(kWhiteColor, BlendMode.srcIn),
-                  image: const AssetImage('assets/love.png'),
+            GestureDetector(
+              onTap: () {
+                print("Triple dot icon");
+              },
+              child: Container(
+                height: 25,
+                width: 25,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    colorFilter: ColorFilter.mode(
+                      kBlackColor,
+                      BlendMode.srcIn,
+                    ),
+                    image: const AssetImage('assets/triple-dot.png'),
+                  ),
                 ),
               ),
-            ),
+            )
           ],
         ),
       );
     }
 
     Widget postContent() {
-      return Container(
-        width: double.infinity,
-        padding: const EdgeInsets.symmetric(
-          vertical: 2,
-          horizontal: 10,
-        ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text.rich(
-              maxLines: 2,
-              overflow: TextOverflow.ellipsis,
-              TextSpan(
-                text: 'acepnurmansidik_ ',
-                style: whiteTextStyle.copyWith(
-                  fontWeight: semibold,
-                  fontSize: 12,
-                  color: kBlackColor,
-                ),
-                children: [
-                  TextSpan(
-                    text:
-                        "sydneyasgdfadahdjadafgdagdfafdgasdhggdasgfdsakdljaskljdjdhasjhjghagdhasdadgafdfdfasdfgafdadakdhljasdhasgdkadadjasgdhasgdjkadasda",
-                    style: whiteTextStyle.copyWith(
-                      fontWeight: medium,
-                      color: kBlackColor,
+      return GestureDetector(
+        onTap: () {
+          setState(() {
+            isShowDesc = !isShowDesc;
+          });
+        },
+        child: Container(
+          width: double.infinity,
+          padding: const EdgeInsets.symmetric(
+            vertical: 2,
+            horizontal: 10,
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              isShowDesc == false
+                  ? Text.rich(
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
+                      TextSpan(
+                        text: 'acepnurmansidik_ ',
+                        style: whiteTextStyle.copyWith(
+                          fontWeight: semibold,
+                          fontSize: 12,
+                          color: kBlackColor,
+                        ),
+                        children: [
+                          TextSpan(
+                            text:
+                                "sydneyasgdfadahdjadafgdagdfafdgasdhggdasgfdsakdljaskljdjdhasjhjghagdhasdadgafdfdfasdfgafdadakdhljasdhasgdkadadjasgdhasgdjkadasda",
+                            style: whiteTextStyle.copyWith(
+                              fontWeight: medium,
+                              color: kBlackColor,
+                            ),
+                          ),
+                        ],
+                      ),
+                    )
+                  : Text.rich(
+                      TextSpan(
+                        text: 'acepnurmansidik_ ',
+                        style: whiteTextStyle.copyWith(
+                          fontWeight: semibold,
+                          fontSize: 12,
+                          color: kBlackColor,
+                        ),
+                        children: [
+                          TextSpan(
+                            text:
+                                "sydneyasgdfadahdjadafgdagdfafdgasdhggdasgfdsakdljaskljdjdhasjhjghagdhasdadgafdfdfasdfgafdadakdhljasdhasgdkadadjasgdhasgdjkadasda",
+                            style: whiteTextStyle.copyWith(
+                              fontWeight: medium,
+                              color: kBlackColor,
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
+              Container(
+                margin: const EdgeInsets.symmetric(vertical: 5),
+                child: Text(
+                  '3 hour ago',
+                  style: greyTextStyle.copyWith(
+                    fontSize: 12,
+                    fontWeight: semibold,
+                    color: kGreyColor.withOpacity(.8),
                   ),
-                ],
-              ),
-            ),
-            Container(
-              margin: const EdgeInsets.symmetric(vertical: 5),
-              child: Text(
-                '3 hour ago',
-                style: greyTextStyle.copyWith(
-                  fontSize: 12,
-                  fontWeight: semibold,
-                  color: kGreyColor.withOpacity(.8),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       );
     }
@@ -139,23 +181,32 @@ class _PostItemState extends State<PostItem> {
             height: 300,
             width: double.infinity,
             color: kPrimaryColor,
-            child: SingleChildScrollView(
-              controller: controller,
-              scrollDirection: Axis.horizontal,
-              child: Row(
-                children: [
-                  Container(
-                    height: double.infinity,
-                    width: MediaQuery.of(context).size.width,
-                    color: kBlackColor,
-                  ),
-                  Container(
-                    height: double.infinity,
-                    width: MediaQuery.of(context).size.width,
-                    color: kWhiteColor,
-                  ),
-                ],
+            child: CarouselSlider(
+              options: CarouselOptions(
+                enableInfiniteScroll: false,
+                pauseAutoPlayInFiniteScroll: false,
+                pauseAutoPlayOnTouch: false,
+                pauseAutoPlayOnManualNavigate: false,
+                viewportFraction: 1,
+                aspectRatio: 1,
+                onPageChanged: (index, reason) {
+                  setState(() {
+                    currentIndex = index;
+                  });
+                },
               ),
+              items: [
+                Container(
+                  height: 300,
+                  width: MediaQuery.of(context).size.width,
+                  color: kBlackColor,
+                ),
+                Container(
+                  height: 300,
+                  width: MediaQuery.of(context).size.width,
+                  color: kPrimaryColor,
+                ),
+              ],
             ),
           ),
           Container(
@@ -189,9 +240,17 @@ class _PostItemState extends State<PostItem> {
       );
     }
 
+    Widget postSingleFile() {
+      return Container(
+        height: 300,
+        width: double.infinity,
+        color: kPrimaryColor,
+      );
+    }
+
     return Container(
       width: double.infinity,
-      color: kSecondaryColor,
+      color: kWhiteColor,
       child: Column(
         children: [
           postHeader(),
@@ -203,14 +262,37 @@ class _PostItemState extends State<PostItem> {
             padding: const EdgeInsets.symmetric(horizontal: 10),
             child: Row(
               children: [
-                iconBtnWithDesc(iconName: "love", counting: "562"),
-                iconBtnWithDesc(iconName: "comment", counting: "23"),
-                iconBtnWithDesc(iconName: "msg", counting: "23"),
+                iconBtnWithDesc(
+                  iconName: "love",
+                  counting: "562",
+                  onPressed: () {
+                    print("Like button");
+                  },
+                ),
+                iconBtnWithDesc(
+                  iconName: "comment",
+                  counting: "23",
+                  onPressed: () {
+                    print("Comment button");
+                  },
+                ),
+                iconBtnWithDesc(
+                  iconName: "msg",
+                  counting: "23",
+                  onPressed: () {
+                    print("Share action");
+                  },
+                ),
                 const Spacer(),
-                IconWrap(
-                  iconName: 'bookmark',
-                  size: 27,
-                  color: kBlackColor,
+                GestureDetector(
+                  onTap: () {
+                    print("Bookmaark button");
+                  },
+                  child: IconWrap(
+                    iconName: 'bookmark',
+                    size: 27,
+                    color: kBlackColor,
+                  ),
                 ),
               ],
             ),
